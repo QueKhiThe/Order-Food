f64dbaec4cf9b38297dd4ab4e379ab147606648d
Que Khi The <dinhtrongque01071996@gmail.com>
2019-07-27 08:08:05 +0700

    Init project

---
+++ Order Food/Order Food/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Order Food
+//
+//  Created by MBA0023 on 7/27/19.
+//  Copyright © 2019 MBA0023. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
